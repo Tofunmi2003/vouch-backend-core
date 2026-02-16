ea7212e16491a7c6f4f46775c6f3d262a8cb36d2
Tofunmi <tohfie2003@gmail.com>
2026-02-16 13:39:39 +0100

    add heartbeat

--- cLw.js
+++ cLw.js
@@ -133,3 +133,14 @@ return;
 }
 
 runEmergencyAudit(0);
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Vouch Backend is Live and Running!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+console.log("Server is listening on port: " + PORT);
+});
